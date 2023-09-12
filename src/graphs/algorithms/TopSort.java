2307912d7a7808deab4572e2e9fe8d0621744ec4
Nathan Andrade <nathan.nhdsk@gmail.com>
2023-09-11 21:13:43 -0300

    root starts from 1 not 0

--- src/graphs/algorithms/TopSort.java
+++ src/graphs/algorithms/TopSort.java
@@ -4,7 +4,7 @@ import src.graphs.Graph;
 
 public class TopSort {
     private Graph graph;
-    private int root = 0;
+    private int root = 1;
     public TopSort(Graph graph) {
         this.graph = graph;
     }
